587f38f0ee4b672efabe6b2904761b277c7e25b4
Mohamed Hamouda <mohamed.hamouda7897@gmail.com>
2023-09-14 21:23:02 +0300

    load hadeth file

--- lib/tabs/ahadeth.dart
+++ lib/tabs/ahadeth.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_c9_mon/hadeth_details.dart';
+import 'package:islami_c9_mon/hadeth_model.dart';
+import 'package:islami_c9_mon/my_theme_data.dart';
 
-class AhadethTab extends StatelessWidget {
-  const AhadethTab({super.key});
+class AhadethTab extends StatefulWidget {
+  @override
+  State<AhadethTab> createState() => _AhadethTabState();
+}
+
+class _AhadethTabState extends State<AhadethTab> {
+  List<HadethModel> allAhadeth = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    loadHadeth();
+    return Column(
+      children: [
+        Image.asset("assets/images/hadeth_bg.png"),
+        Divider(
+          thickness: 3,
+          color: MyThemeData.primary,
+        ),
+        Text(
+          "Ahadeth",
+        ),
+        Divider(
+          thickness: 3,
+          color: MyThemeData.primary,
+        ),
+        Expanded(
+          child: ListView.separated(
+            separatorBuilder: (context, index) => Divider(
+              thickness: 1,
+              endIndent: 40,
+              indent: 40,
+              color: MyThemeData.primary,
+            ),
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, HadethDetails.routeName,
+                      arguments: allAhadeth[index]);
+                },
+                child: Text(
+                  allAhadeth[index].title,
+                  textAlign: TextAlign.center,
+                ),
+              );
+            },
+            itemCount: allAhadeth.length,
+          ),
+        )
+      ],
     );
+  }
+
+  // String name="  Moha \n med  " >> "Moha  med"
+  loadHadeth() async {
+    rootBundle.loadString("assets/files/ahadeth.txt").then((ahadeth) {
+      List<String> ahadethList = ahadeth.split("#");
+      for (int i = 0; i < ahadethList.length; i++) {
+        String hadethOne = ahadethList[i];
+        List<String> hadethOneLines = hadethOne.trim().split("\n");
+        String title = hadethOneLines[0];
+        hadethOneLines.removeAt(0);
+        List<String> content = hadethOneLines;
+        HadethModel hadethModel = HadethModel(title, content);
+        allAhadeth.add(hadethModel);
+
+        print(title);
+      }
+      setState(() {});
+    }).catchError((e) {
+      print(e.toString());
+    });
   }
 }
